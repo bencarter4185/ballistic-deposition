6b6ebc02880db82202cb10c73cb4ef402bf3a0d1
bencarter4185 <bencarter4185@gmail.com>
2021-08-24 17:05:56 +0100

    Add parsing of `config.ini` file

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,35 @@
 /*
 Code to perform ballistic deposition experiments in Rust.
 
+Change values within config.ini to perform memes
+
 The code requires no user input, and all variables are defined as constants below. 
 */
 
 mod sim;
+
+use std::error::Error;
 use sim::{T_MAX, SKIP_VALS};
 
-// extern crate dotenv;
+fn main() -> Result<(), Box<dyn Error>> {
+    // println!("{:?}", T_MAX[&8.to_string()]);
+    // println!("{:?}", SKIP_VALS[&8.to_string()]);
 
-#[macro_use]
-extern crate dotenv_codegen;
+    // Open the config file
+    let config = sim::open_config().expect("Failed to open `config.ini`.");
 
-use dotenv::dotenv;
-use std::env;
+    // Parse the config file for its contents
+    let _l_vals: Vec<u32> = sim::parse_config_param(&config, "simulation_params", "substrate_lengths")
+        .expect("Failed to parse substrate lengths.");
+    let _seeds: Vec<u32> = sim::parse_config_param(&config, "simulation_params", "seeds")
+        .expect("Failed to parse seeds.");
+    let _angles: Vec<u32> = sim::parse_config_param(&config, "simulation_params", "angles")
+        .expect("Failed to parse angles.");
+    let _imp_rec: Vec<u32> = sim::parse_config_param(&config, "simulation_params", "impurity_recurrence")
+        .expect("Failed to parse impurity recurrences.");
 
-fn main() {
-    println!("Hello, world!");
-    sim::run_sim();
+    let _imp_add: bool = sim::parse_config_option(&config, "options", "add_impurities")
+        .expect("Failed to parse whether to add impurities.");
 
-    println!("{:?}", T_MAX[&8.to_string()]);
-    println!("{:?}", SKIP_VALS[&8.to_string()]);
-
-    // dotenv().ok();
-
-    // // for (key, value) in env::vars() {
-    // //     println!("{}: {}", key, value);
-    // // }
-
-    println!("{}", dotenv!("VAR"));
+    Ok(())
 }
--- src/sim.rs
+++ src/sim.rs
@@ -1,4 +1,15 @@
+/*
+Library file used for running the ballistic deposition simulations.
+*/
+
+extern crate phf;
+use std::{error::Error, io::Split, vec};
+
 use phf::phf_map;
+
+extern crate configparser;
+use configparser::ini::Ini;
+
 
 // Map the maximum value of `t` for each substrate length `L` 
 pub static T_MAX: phf::Map<&'static str, i64> = phf_map! {
@@ -27,6 +38,57 @@ pub static SKIP_VALS: phf::Map<&'static str, i64> = phf_map! {
     "2048" => 12800,
     "4096" => 25600,
 };
+
+pub fn open_config() -> Result<Ini, Box<dyn Error>> {
+    // Open the `config.ini` file and extract the contents
+
+    let mut config = Ini::new();
+    config.load("config.ini")?;
+    
+    Ok(config)
+}
+
+pub fn parse_config_param(config: &Ini, section: &str, key: &str) -> Result<Vec<u32>, Box<dyn Error>> {
+    // Parse the config.ini file for its specified key value pair.
+    // This code works for parsing a vector of values only.
+    let config_entry = &config
+        .get(section, key)
+        .expect("Invalid section/key pair in `config.ini`.")[..]; // Convert to string literal
+
+    let mut vals: Vec<u32> = vec![];
+
+    let values = config_entry.split(',');
+    for v in values {
+        let temp: u32 = v.trim().parse().unwrap_or(0);
+        vals.push(temp);
+    }
+
+    Ok(vals)
+}
+
+pub fn parse_config_option(config: &Ini, section: &str, key: &str) -> Result<bool, Box<dyn Error>> {
+    // Parse the config.ini file for its specified key value pair.
+    // This code works for parsing boolean values only.
+    let config_entry = &config
+        .get(section, key)
+        .expect("Invalid section/key pair in config.ini.")[..]; // convert to string literal &str
+
+    let aliases_true: Vec<&str> = vec!["true", "True", "y"];
+    let aliases_false: Vec<&str> = vec!["false", "False", "n"];
+
+    let val: bool = match config_entry {
+        config_entry if aliases_true.iter().any(|&i| i == config_entry) => true,
+        config_entry if aliases_false.iter().any(|&i| i == config_entry) => false,
+        _ => {
+            println!(r"WARNING: The entry for `{}, {}` is malformed. Setting value to `false` by default.
+            Please specify one of {:?} for `true`,
+            or one of {:?} for `false`.", section, key, aliases_true, aliases_false);
+            false
+        }, 
+    };
+
+    Ok(val)
+}
 
 pub fn run_sim() {
     println!("Running simulation...");
